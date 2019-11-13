510d2b6201a4bdcdb14e2a529cfeb6a434c0cb00
magdaavram <magdahalus@gmail.com>
2019-11-13 16:41:22 +0200

    Remove old task injection method

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -4,7 +4,7 @@ let newTask;
 
 $(".create-button").click(() => {
     newTask = getTask(".create-input");
-    createTask(newTask, 5, "12/11, 10:33");
+    console.log(newTask);
 });
 
 
@@ -12,7 +12,7 @@ $(".create-input").keydown((ev) => {
     if (ev.key === "Enter") {
         ev.preventDefault();
         newTask = getTask(".create-input");
-        createTask(newTask, 4, "12/11, 11:21");
+        console.log(newTask);
     }
 });
 
@@ -23,18 +23,4 @@ function getTask(taskInputId) {
     taskInput.val("");
 
     return newTask;
-}
-
-function createTask(taskName, taskId, date) {
-    $(`<div class="custom-control custom-checkbox mr-sm-2 task-group d-flex justify-content-between">
-<input class="custom-control-input" id="task-${taskId}" type="checkbox">
-<label class="custom-control-label task-container" for="task-${taskId}">
-<div class="d-flex align-items-center">
-<span>${taskName}</span>
-<span class="date-created text-center">(${date})</span>
-</div>
-<input class="form-control d-none" name="task-${taskId}" type="text"></label>
-<button attr-btn="task-${taskId}" class="btn btn-danger btn-sm" type="button">X</button>
-</div>`)
-        .appendTo(".task-inputs");
 }
