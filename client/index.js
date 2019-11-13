5cea0ddf978447302a78f03724e28b8a35418758
magdaavram <magdahalus@gmail.com>
2019-11-13 12:25:46 +0200

    Implement insert task in list functionality

--- client/index.js
+++ client/index.js
@@ -1,15 +1,17 @@
-$(document).ready(() => {
-    $(".create-button").click(() => {
-        getTask(".create-input");
-    });
+let newTask;
+
+$(".create-button").click(() => {
+    newTask = getTask(".create-input");
+    createTask(newTask, 3, "12/11, 10:33");
+});
 
 
-    $(".create-input").keydown((ev) => {
-        if (ev.key === "Enter") {
-            ev.preventDefault();
-            getTask(".create-input");
-        }
-    });
+$(".create-input").keydown((ev) => {
+    if (ev.key === "Enter") {
+        ev.preventDefault();
+        newTask = getTask(".create-input");
+        createTask(newTask, 4, "12/11, 11:21");
+    }
 });
 
 
@@ -18,6 +20,19 @@ function getTask(taskInputId) {
     const newTask = taskInput.val();
     taskInput.val("");
 
-    console.log(newTask);
     return newTask;
+}
+
+function createTask(taskName, taskId, date) {
+    $(`<div class="custom-control custom-checkbox mr-sm-2 task-group">
+<input class="custom-control-input" id="task-${taskId}" type="checkbox">
+<label class="custom-control-label task-container" for="task-${taskId}">
+<div>
+<span>${taskName}</span>
+<span class="date-created">(${date})</span>
+</div>
+<input class="form-control no-display" name="task-${taskId}" type="text"></label>
+<button attr-btn="task-${taskId}" class="btn btn-danger btn-sm" type="button">X</button>
+</div>`)
+        .appendTo(".task-inputs");
 }
