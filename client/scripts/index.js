0e7b2757e7f953dce5ba3e082558943c68ba6c20
magdaavram <magdahalus@gmail.com>
2019-11-14 12:20:39 +0200

    Implement get date method

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -6,16 +6,20 @@
 let newTask;
 
 $(".create-button").click(() => {
+    const date = getDate();
+
     newTask = getTask(".create-input");
-    console.log(newTask);
+    console.log(newTask, date);
 });
 
 
 $(".create-input").keydown((ev) => {
     if (ev.key === "Enter") {
+        const date = getDate();
+
         ev.preventDefault();
         newTask = getTask(".create-input");
-        console.log(newTask);
+        console.log(newTask, date);
     }
 });
 
@@ -26,4 +30,14 @@ function getTask(taskInputId) {
     taskInput.val("");
 
     return newTask;
+}
+
+function getDate() {
+    const currentDate = new Date();
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth() + 1;
+    const hour = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+
+    return `${day}/${month}, ${hour}:${minutes}`;
 }
