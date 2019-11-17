2f091a5d107838f49f630df6a85c4332e1848eb2
Magda Avram <magdahalus@gmail.com>
2019-11-17 11:03:28 +0200

    Create distinct methods for rendering one or more tasks

---
+++ client/scripts/view.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Mustache = require('mustache');
+
+const renderTask = (task) => {
+    const taskTemplate = $('#task-template').html();
+
+    $('.task-inputs').append(Mustache.render(taskTemplate, task));
+};
+
+const renderTasks = (tasks) => {
+    const taskTemplate = $('#task-template').html();
+
+    $.each(tasks, (index, task) => {
+        $('.task-inputs').append(Mustache.render(taskTemplate, task));
+    });
+};
+
+export {renderTask, renderTasks}
